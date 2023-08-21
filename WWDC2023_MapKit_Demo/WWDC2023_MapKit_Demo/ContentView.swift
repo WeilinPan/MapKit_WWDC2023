e18769fe16f91c0e259c96f37d910796a05db345
WeilinPan <50310307+WeilinPan@users.noreply.github.com>
2023-08-21 23:18:42 +0800

    feat: project init

---
+++ WWDC2023_MapKit_Demo/WWDC2023_MapKit_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC2023_MapKit_Demo
+//
+//  Created by 潘威霖 on 2023/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC2023_MapKit_Demo/WWDC2023_MapKit_Demo/WWDC2023_MapKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2023_MapKit_DemoApp.swift
+//  WWDC2023_MapKit_Demo
+//
+//  Created by 潘威霖 on 2023/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2023_MapKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
